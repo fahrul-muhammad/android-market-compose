4e67c8ff91ea4206c62872862543d2a622568406
fahrul-muhammad <fahrulmuh23@gmail.com>
2024-10-04 21:10:17 +0800

    layouting top menu done

--- app/src/main/java/com/gunder/market/MainActivity.kt
+++ app/src/main/java/com/gunder/market/MainActivity.kt
@@ -5,15 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gunder.market.components.MainTopBar
+import com.gunder.market.components.TopMenu
+import com.gunder.market.model.dummyListTopBar
 import com.gunder.market.ui.theme.MarketTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,11 +32,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MarketApp()
+                        MarketApp()
+                    }
                 }
             }
         }
-    }
 }
 
 @Composable
@@ -40,11 +46,29 @@ fun MarketApp(modifier: Modifier = Modifier) {
             .verticalScroll(rememberScrollState())
     ) {
         MainTopBar()
+        MainTopMenu()
     }
 
 }
 
-@Preview(device = Devices.DEFAULT, showBackground = true)
+@Composable
+fun MainTopMenu() {
+    LazyRow(modifier = Modifier.padding(4.dp)) {
+        items(dummyListTopBar){
+            TopMenu(listTopMenu = it)
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun MainTopMenuPreview() {
+    MarketTheme {
+        MainTopMenu()
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
 @Composable
 fun MarketAppPreview() {
     MarketTheme {
